4ef50775d420f1756b69bd2f1e1c299a3eaca584
rajat-gith <rajatnayak1582002@gmail.com>
2025-07-07 09:58:17 +0530

    commented the connect to mongodb implementation

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const config = require("./src/config");
 const connectDB = require("./src/utils/db"); 
 
 // Connect to MongoDB
-connectDB();
+// connectDB();
 
 app.listen(config.port, () => {
 	console.log(`Server running on http://localhost:${config.port}`);
