4d788d913c296508957c1079f41b2045821f4a85
rajat-gith <rajatnayak1582002@gmail.com>
2025-11-14 01:09:00 +0530

    worked on db changes

--- src/config/index.js
+++ src/config/index.js
@@ -8,6 +8,19 @@ module.exports = {
 		clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		redirectUri: process.env.GOOGLE_REDIRECT_URI,
 	},
+	db: {
+		user: process.env.PG_USER,
+		host: process.env.PG_HOST,
+		database: process.env.PG_DB,
+		password: process.env.PG_PASS,
+		port: process.env.PG_PORT || 5432,
+		ssl: process.env.PG_CA
+			? {
+					ca: process.env.PG_CA,
+					rejectUnauthorized: true,
+			  }
+			: false, // disables SSL locally if no CA provided
+	},
 	jwt: {
 		secret: process.env.JWT_SECRET,
 		expiresIn: "1h",
--- src/db/index.js
+++ src/db/index.js
@@ -1,19 +1,13 @@
 const { Pool } = require("pg");
-require("dotenv").config();
+const config = require("../config");
 
-const pool = new Pool({
-	user: process.env.PG_USER,
-	host: process.env.PG_HOST,
-	database: process.env.PG_DB,
-	password: process.env.PG_PASS,
-	port: 5432,
-});
+const pool = new Pool(config.db);
 
 pool.query("SELECT NOW()", (err, res) => {
 	if (err) {
-		console.error("❌ Database connection failed:", err.stack);
+		console.error("Database connection failed:", err.stack);
 	} else {
-		console.log("✅ Database connected successfully at:", res.rows[0].now);
+		console.log("Connected to DB at:", res.rows[0].now);
 	}
 });
 
